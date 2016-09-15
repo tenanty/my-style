1c67c2441fa69ae44c37ad8b960610ff20f4374a
yuanshun <tenanty@gmail.com>
2016-09-15 22:26:07 +0800

    本地提交

---
+++ src/main/java/com/channelsoft/mystyle/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.channelsoft.mystyle.controller;
+
+/**
+ * <dl>
+ * <dt>${TYPE_NAME}</dt>
+ * <dd>Description:${TODO}(类描述)</dd>
+ * <dd>Copyright:Copyright (C) 2016</dd>
+ * <dd>Company:青牛(北京)技术有限公司</dd>
+ * <dd>Createtime:16/9/15</dd>
+ * </dl>
+ *
+ * @author yuanshun
+ */
+public class LoginController {
+
+}
